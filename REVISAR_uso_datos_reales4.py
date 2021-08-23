df8c91d4a569f79430eb40a8fa450a11d62369f1
Gabriela Badi <43677786+OAVVgab@users.noreply.github.com>
2021-08-23 11:53:28 -0300

    Add files via upload

---
+++ REVISAR_uso_datos_reales4.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Autora: María Celeste Novak Merquel
+"""
+
+import pandas as pd
+import numpy as np
+from obspy.core import read
+from obspy import UTCDateTime
+import os
+import matplotlib.pyplot as plt
+
+
+# Leo los tags
+# Primer fila del archivo: fecha,evento,duracion,amplitud --> indica el orden
+# de las columnas. Las comas delimitan las columnas
+tags_file = '/media/gaby/Backup Plus/SISMOLOGIA DATOS Y DOCS/Vn_FUEGO/tags/fecha_tipo_dur_ampl_max_LP_2020.csv'
+
+# Genero una variable tipo DataFrame con los datos de tags_file, le indico que 
+# las comas separan columnas y que en la fila=0 está el header
+tags = pd.read_csv(tags_file,sep=',',header=0)
+
+
+# Extraigo vectores de datos y los pongo en variables
+dates = tags.fecha
+tipos = tags.tipo
+duracion = tags.dur
+
+# Leo un archivo de señal
+#wave_file = 'C:/Users/Propietario/Desktop/tesis_de_grado/ATENUACION/DATOS/FG16/GI.FG16.00.BHE.D.2020.062'
+
+
+# A partir de los tags, se genera el nombre de la forma de onda a partir de una
+# única estación (FG16). La raiz del nombre de archivo debe cambiarse de 
+# acuerdo a la estructura de carpetas en la maquina donde esten los datos. P. ej:
+# wave_file='/mnt/hgfs/Compartida_Ubuntu/DATOS/Trazas_Fuego/GI.FG16.00.BHZ.D.'
+
+dir_base='/media/gaby/Backup Plus/SISMOLOGIA DATOS Y DOCS/Vn_FUEGO/FG16'
+'''
+armo el listado de archivos de datos en la base
+'''
+lista_nombres=os.listdir(dir_base)
+
+# De dates extraigo año y día juliano para convertirlos en cadenas de caracteres
+# y usarlo para completar el nombre del wave_file
+
+
+
+for i in range(0,len(dates),1):
+    inicio = UTCDateTime(dates[i])
+    anio=inicio.year
+    dia=inicio.julday
+    hora=inicio.hour
+    minuto=inicio.minute
+    segundo=inicio.second
+
+# Se genera un diccionario con toda la información    
+    dates_array=np.array([anio, dia, hora, minuto ,segundo])
+    newdates=pd.DataFrame(dates_array,columns=['str'])
+    newdates['str'] = newdates['str'].apply(str)    
+
+# Se recorre de inicio a fin cada evento y se lo distingue según su tipo
+    #for i in range(6854,len(dates)):
+    wave_file = os.path.join(dir_base,'GI.FG16.00.BHZ.D.')
+    inicio=inicio-5
+    diaj = newdates.str[1]
+    if len(diaj)==1:
+        dayofyear='00'+diaj
+    elif len(diaj)==2:
+        dayofyear='0'+diaj
+    else:
+        dayofyear=diaj
+    wave_file +=newdates.str[0]+'.'+dayofyear
+   # print(wave_file)
+    for h in range(len(lista_nombres)):
+       # print(h)
+        lista_nombres2 = os.path.join(dir_base,lista_nombres[h])
+       # print(lista_nombres2)
+        if wave_file != lista_nombres2:
+            continue
+        else:
+            wave=read(wave_file)
+            evento=wave.slice(inicio,inicio+duracion[i])
+            print('encontrado'+lista_nombres2)
+            traza_evento=evento[0]
+            dato=traza_evento.data
+            evento.plot()
+            input('presione una tecla...')
+            break
+    #print('ya cargue archivo y plotee')
+        
+
